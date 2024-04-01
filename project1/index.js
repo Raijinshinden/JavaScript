7c22d1c8c16f24ce1d89cf6a520d0e5157025a63
Ryan Dempsey <rjd9896@gmail.com>
2024-04-02 00:51:53 +0900

    Update index.js

--- project1/index.js
+++ project1/index.js
@@ -126,9 +126,33 @@ let username;
 //Changing the datatype of a value to another.
 //(strings, numbers, booleans)
 
+/*
 let age = window.prompt("How old are you?");
 
 age = Number(age);
 age+=1;
 
 console.log(age, typeof age);
+*/
+
+/*
+
+let x = "pizza";
+let y = "pizza";
+let z = "pizza";
+
+x = Number(x);
+y = String(y);
+z = Boolean(z);
+
+console.log(x, typeof x);
+console.log(y, typeof y);
+console.log(z, typeof z);
+
+*/
+
+//Const = a variable that can't be changed;
+
+let pi = 3.14159;
+let radius;
+let circumference;
