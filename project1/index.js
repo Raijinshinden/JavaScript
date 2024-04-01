8699860fd18b74689f366e9a6fd2c05e4bca1bac
Ryan Dempsey <rjd9896@gmail.com>
2024-04-01 23:38:12 +0900

    edits

--- project1/index.js
+++ project1/index.js
@@ -1,5 +1,21 @@
-console.log(`Hello`);
-console.log(`I like Pizza!`);
+//console.log(`Hello`);
+//console.log(`I like Pizza!`);
 
-window.alert(`This is an alert!`);
-window.alert(`I like Pizza!`);
+//window.alert(`This is an alert!`);
+//window.alert(`I like Pizza!`);
+
+//document.getElementById("myH1").textContent = "Hello"; //We first have to select the element, then change the text content.
+//document.getElementById("myP").textContent = "I like pizza!"; //We first have to select the element, then change the text content.
+
+//This is a comment
+
+/* 
+This 
+is 
+a 
+multi-line
+comment
+*/
+
+//Variables = A container that stores a value.
+//Behaves as if it were the value it contains.
