bc7902c644ccf93788b567d550fc012ea356fc35
Ryan Dempsey <rjd9896@gmail.com>
2024-04-01 23:46:35 +0900

    Update index.js

--- project1/index.js
+++ project1/index.js
@@ -28,6 +28,7 @@ let age = 25;
 let price = 10.99;
 let gpa = 2.1;
 
-console.log(age);
-console.log(price);
-console.log(gpa);
+//Using a template literal, we can insert a variable using a placeholder.
+console.log(`You are ${age} years old.`);
+console.log(`The price is $${price}.`);
+console.log(`Your gpa is: ${gpa}`);
