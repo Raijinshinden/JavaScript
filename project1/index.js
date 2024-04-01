7889b2b512fdd04703f66c6b0ecb216311b238f0
Ryan Dempsey <rjd9896@gmail.com>
2024-04-02 00:38:39 +0900

    edit

--- project1/index.js
+++ project1/index.js
@@ -117,5 +117,5 @@ let username;
 
 document.getElementById("mySubmit").onclick = function() {
     username = document.getElementById("myText").value;
-    console.log(username);
+    document.getElementById("myH1").textContent = `Hello ${username}.`
 };
