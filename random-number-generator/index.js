2c86f9573e7414373e1fae70b8f0bd9c7838fd2d
Ryan Dempsey <rjd9896@gmail.com>
2024-04-02 22:36:14 +0900

    Update index.js

--- random-number-generator/index.js
+++ random-number-generator/index.js
@@ -1,6 +1,13 @@
 
 // Random Number Generator
 
-let randomNum = Math.random();
+//let randomNum = Math.floor(Math.random() * 6) + 1;
 
-console.log(randomNum);
+//console.log(randomNum);
+
+//If we are looking for a random number between a range,
+
+const min = 50;
+const maz = 100;
+
+let randomNum = Math.floor(Math.random() * (Max - min)) + min;
