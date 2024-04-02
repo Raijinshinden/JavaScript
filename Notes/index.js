e4a64c982aca52d0704bec8188a8696e0490c54c
Ryan Dempsey <rjd9896@gmail.com>
2024-04-02 23:49:52 +0900

    Update index.js

--- Notes/index.js
+++ Notes/index.js
@@ -271,4 +271,17 @@ mySubmit.onclick = function() {
     else{
         subResult.textContent = `You are NOT subscribed!`;
     }
+
+    if(visaBtn.checked){
+        paymentResult.textContent = `You are paying with Visa.`
+    }
+    else if(masterCardBtn.checked){
+        paymentResult.textContent = `You are paying with MasterCard.`
+    }
+    else if(payPalBtn.checked){
+        paymentResult.textContent = `You are paying with PayPal.`
+    }
+    else{
+        paymentResult.textContent = `You must select a payment type.`
+    }
 }
