f13c33123eca44d4e7f5a80d3d325eaacfc1ee42
Ryan Dempsey <rjd9896@gmail.com>
2024-04-02 23:54:13 +0900

    Update index.js

--- Notes/index.js
+++ Notes/index.js
@@ -256,6 +256,7 @@ mySubmit.onclick = function(){
 //.checked = property that determines the checked state of an
 //           HTML checkbox or radio button element.
 
+/*
 const myCheckbox = document.getElementById("myCheckbox");
 const visaBtn = document.getElementById("visaBtn");
 const masterCardBtn = document.getElementById("masterCardBtn");
@@ -273,15 +274,21 @@ mySubmit.onclick = function() {
     }
 
     if(visaBtn.checked){
-        paymentResult.textContent = `You are paying with Visa.`
+        paymentResult.textContent = `You are paying with Visa.`;
     }
     else if(masterCardBtn.checked){
-        paymentResult.textContent = `You are paying with MasterCard.`
+        paymentResult.textContent = `You are paying with MasterCard.`;
     }
     else if(payPalBtn.checked){
-        paymentResult.textContent = `You are paying with PayPal.`
+        paymentResult.textContent = `You are paying with PayPal.`;
     }
     else{
-        paymentResult.textContent = `You must select a payment type.`
+        paymentResult.textContent = `You must select a payment type.`;
     }
 }
+*/
+
+//ternary operator = a shortcut to if() and else() statements
+//                   helps to assign a variable based on a condition
+//                   condition ? codeIfTrue : codeIfFalse;
+
