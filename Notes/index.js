2343f37b84bf44c1bc4f5185286e551932b05b76
Ryan Dempsey <rjd9896@gmail.com>
2024-04-03 00:01:27 +0900

    Update index.js

--- Notes/index.js
+++ Notes/index.js
@@ -292,6 +292,12 @@ mySubmit.onclick = function() {
 //                   helps to assign a variable based on a condition
 //                   condition ? codeIfTrue : codeIfFalse;
 
-let age = 21;
-let message = age >= 18 ? "You're an adult" : "You're a minor";
-console.log(message);
+//let age = 21;
+//let message = age >= 18 ? "You're an adult" : "You're a minor";
+//console.log(message);
+
+//let time = 16;
+//let greeting = time < 12 ? "Good Morning!" : "Good Afternoon";
+//console.log(greeting);
+
+//
