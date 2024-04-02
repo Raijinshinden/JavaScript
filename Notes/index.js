b6be9f45c2891c18d8f6e202526813fd0fdafbbd
Ryan Dempsey <rjd9896@gmail.com>
2024-04-02 23:45:25 +0900

    Update index.js

--- Notes/index.js
+++ Notes/index.js
@@ -260,3 +260,15 @@ const myCheckbox = document.getElementById("myCheckbox");
 const visaBtn = document.getElementById("visaBtn");
 const masterCardBtn = document.getElementById("masterCardBtn");
 const payPalBtn = document.getElementById("payPalBtn");
+const mySubmit = document.getElementById("mySubmit");
+const subResult = document.getElementById("subResult");
+const paymentResult = document.getElementById("paymentResult");
+
+mySubmit.onclick = function() {
+    if(myCheckbox.checked){
+        subResult.textContent = `You are subscribed!`;
+    }
+    else{
+        subResult.textContent = `You are not subscribed!`;
+    }
+}
