8483cc2f492854299cff47a3fb6d0c5f405cd913
Ryan Dempsey <rjd9896@gmail.com>
2024-04-02 22:58:47 +0900

    Update index.js

--- Notes/index.js
+++ Notes/index.js
@@ -205,3 +205,20 @@ z = Math.round(x);
 
 //IF Statements = if a condition is true, execute some code
 //                if not, do something else.
+
+let age = 25;
+
+if(age >= 18) {
+    console.log("You are old enough to enter this site.")
+}else{
+    console.log("You must by 18+ to enter this site.")
+}
+
+
+let time = 9;
+
+if(time < 12) {
+    console.log("Good Morning.")
+}else{
+    console.log("Good Afternoon.")
+}
