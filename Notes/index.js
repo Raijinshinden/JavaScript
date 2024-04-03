bcd356fced0162b4ba8b7dd5927a32e5470a526c
Ryan Dempsey <rjd9896@gmail.com>
2024-04-04 01:19:46 +0900

    Update index.js

--- Notes/index.js
+++ Notes/index.js
@@ -628,10 +628,18 @@ function openFridge(...foods){
     console.log(foods);
 }
 
+function getFood(...foods){
+    return foods;
+}
+
 const food1 = "pizza";
 const food2 = "hamburger";
 const food3 = "hotdog";
 const food4 = "sushi";
 const food5 = "ramen";
 
-openFridge(food1, food2, food3, food4, food5);
+//openFridge(food1, food2, food3, food4, food5);
+
+const foods = getFood(food1, food2, food3, food4, food5);
+
+console.log(foods);
