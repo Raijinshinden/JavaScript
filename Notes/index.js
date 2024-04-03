80cbae97efa2ee322a48221a038142a08b435552
Ryan Dempsey <rjd9896@gmail.com>
2024-04-03 23:56:05 +0900

    Update index.js

--- Notes/index.js
+++ Notes/index.js
@@ -504,3 +504,17 @@ for(let i = 1; i <= 20; i++){
     }
 }
 */
+
+// function = A section of reusable code.
+//            Declare code once, use it whenever you want.
+//            Call the function to execute that code.
+
+function happyBirthday(){
+    console.log("Happy Birthday to you!");
+    console.log("Happy Birthday to you!");
+    console.log(`Happy Birthday dear ${username}!`);
+    console.log("Happy Birthday to you!");
+    console.log(`You are ${age} years old.`);
+}
+
+happyBirthday();
