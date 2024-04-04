7494aa20989fee0d4d56519decdf23e869cea626
Ryan Dempsey <rjd9896@gmail.com>
2024-04-05 01:08:34 +0900

    Update index.js

--- Notes/index.js
+++ Notes/index.js
@@ -864,7 +864,7 @@ function hello(){
     console.log("Hello");
 }
 
-
+//
 const hello = (name, age) => {console.log(`Hello ${name}`)
                               console.log(`You are ${age} years old. That's hella old fam.`)};
 
@@ -872,10 +872,15 @@ hello("Ryan", 27);
 
 setTimeout(() => console.log("Hello"), 3000);
 */
+//
 const numbers = [1, 2, 3, 4, 5, 6];
 
 const squares = numbers.map((element) => Math.pow(element, 2));
 const cubes = numbers.map((element) => Math.pow(element, 3));
+const evenNums = numbers.filter((element) => element % 2 === 0);
+const oddNums = numbers.filter((element) => element % 2 !== 0);
 
 console.log(squares);
 console.log(cubes);
+console.log(evenNums);
+console.log(oddNums);
