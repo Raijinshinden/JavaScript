326f7848cd73d302549c147e019b5d328bb659cd
Ryan Dempsey <rjd9896@gmail.com>
2024-04-04 23:13:23 +0900

    edits

--- Dice-roller/index.js
+++ Dice-roller/index.js
@@ -10,6 +10,9 @@ function rollDice(){
 
     for(let i = 0; i < numOfDice; i++){
         const value = Math.floor(Math.random() * 6) + 1;
-        console.log(value);
+        values.push(value);
+        images.push(`<img src="images/dice-${value}.png">`)
     }
+    diceResult.textContent = `dice: ${values.join(', ')}`;
+    diceImages.innerHTML = images.join('')
 }
