a71c36a95483f4245c90b2c10b2c2c2af004e965
Ryan Dempsey <rjd9896@gmail.com>
2024-04-05 01:06:09 +0900

    Update index.js

--- Notes/index.js
+++ Notes/index.js
@@ -875,5 +875,7 @@ setTimeout(() => console.log("Hello"), 3000);
 const numbers = [1, 2, 3, 4, 5, 6];
 
 const squares = numbers.map((element) => Math.pow(element, 2));
+const cubes = numbers.map((element) => Math.pow(element, 3));
 
 console.log(squares);
+console.log(cubes);
