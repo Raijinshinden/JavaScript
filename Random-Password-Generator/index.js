5a171cc182e08e22ffc7043c7a8411c262c1c09e
Ryan Dempsey <rjd9896@gmail.com>
2024-04-04 23:37:56 +0900

    edits

---
+++ Random-Password-Generator/index.js
@@ -0,0 +1,36 @@
+
+
+function generatePassword(length, includeLowercase, includeUppercase, includeNumbers, includeSymbols){
+
+    const lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
+    const uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    const numberChars = "0123456789";
+    const symbolChars = "!#$%^<>?|*&+_=-";
+
+    let allowedChars = "";
+    let password = "";
+
+    allowedChars += includeLowercase ? lowercaseChars : "";
+    allowedChars += includeUppercase ? uppercaseChars : "";
+    allowedChars += includeNumbers ? numberChars : "";
+    allowedChars += includeSymbols ? symbolChars : "";
+
+    console.log(allowedChars);
+
+    return '';
+}
+
+
+const passwordLength = 12;
+const includeLowercase = true;
+const includeUppercase = true;
+const includeNumbers = true;
+const includeSymbols = true;
+
+const password = generatePassword(passwordLength, 
+                                  includeLowercase, 
+                                  includeUppercase, 
+                                  includeNumbers, 
+                                  includeSymbols);
+
+console.log(`Generated Password: ${password} `)
