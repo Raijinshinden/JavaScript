4ff5cf7c76465b79fa1f6de2450f9370b13c2a6a
Ryan Dempsey <rjd9896@gmail.com>
2024-04-05 00:33:15 +0900

    Update index.js

--- Notes/index.js
+++ Notes/index.js
@@ -747,7 +747,7 @@ function square(element){
 function cube(element){
     return Math.pow(element, 3);
 }
-*/
+
 const students = ["Spongebob", "Patrick", "Squidward", "Rick James"];
 const studentsUpper = students.map(upperCase);
 const studentsLower = students.map(lowerCase);
@@ -762,3 +762,42 @@ function upperCase(element){
 function lowerCase(element){
     return element.toLowerCase();
 }
+*/
+
+// .filter() = creates a new array by filtering
+//             out elements
+/*
+let numbers = [1, 2, 3, 4, 5, 6, 7];
+let evenNums = numbers.filter(isEven);
+let oddNums = numbers.filter(isOdd);
+
+console.log(evenNums)
+console.log(oddNums)
+
+function isEven(element){
+    return element % 2 === 0;
+}
+
+function isOdd(element){
+    return element % 2 !== 0;
+}
+
+
+const ages = [16, 17, 18, 18, 19, 20, 60];
+const adults = ages.filter(isAdult);
+const children = ages.filter(isChild);
+
+console.log(adults);
+console.log(children);
+
+function isAdult(element){
+    return element >= 18;
+}
+
+function isChild(element){
+    return element < 18;
+}
+*/
+
+
+// 
