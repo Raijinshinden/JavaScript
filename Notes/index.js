b8287de29eed1b925085097ed38eb85dbf0d7737
Ryan Dempsey <rjd9896@gmail.com>
2024-04-05 00:07:13 +0900

    Update index.js

--- Notes/index.js
+++ Notes/index.js
@@ -698,11 +698,23 @@ function displayConsole(result){
 
 let numbers = [1, 2, 3, 4, 5];
 
-numbers.forEach(double);
+numbers.forEach(cube);
 numbers.forEach(display);
 
 function double(element, index, array){
     array[index] = element * 2;
+}
+
+function triple(element, index, array){
+    array[index] = element * 3;
+}
+
+function square(element, index, array){
+    array[index] = Math.pow(element, 2);
+}
+
+function cube(element, index, array){
+    array[index] = Math.pow(element, 3);
 }
 
 function display(element){
