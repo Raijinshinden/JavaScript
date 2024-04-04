fdac5536102e21ac151386fc70d92df36a98bb4f
Ryan Dempsey <rjd9896@gmail.com>
2024-04-05 01:05:19 +0900

    Update index.js

--- Notes/index.js
+++ Notes/index.js
@@ -863,8 +863,17 @@ setTimeout(function(){
 function hello(){
     console.log("Hello");
 }
+
+
+const hello = (name, age) => {console.log(`Hello ${name}`)
+                              console.log(`You are ${age} years old. That's hella old fam.`)};
+
+hello("Ryan", 27);
+
+setTimeout(() => console.log("Hello"), 3000);
 */
+const numbers = [1, 2, 3, 4, 5, 6];
 
-const hello = () => console.log("Hello");
+const squares = numbers.map((element) => Math.pow(element, 2));
 
-hello();
+console.log(squares);
