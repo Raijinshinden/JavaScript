4a33f62184f311ddd86c328163b3569f18b5531c
Ryan Dempsey <rjd9896@gmail.com>
2024-04-05 01:34:59 +0900

    Update index.js

--- Notes/index.js
+++ Notes/index.js
@@ -920,4 +920,28 @@ person2.eat();
 */
 
 
-//
+// this = reference to the object where THIS is used
+//        (the object depends on the immediate context)
+//        person.name = this.name
+
+/*
+const person1 = {
+    name: "Spongebob",
+    favFood: "hamburgers",
+    sayHello: function(){console.log(`Hi, I'm ${this.name}`)},
+    eat: function(){console.log(`${this.name} is eating ${this.favFood}.`)}
+}
+
+const person2 = {
+    name: "Patrick",
+    favFood: "pizza",
+    sayHello: function(){console.log(`Hi, I'm ${this.name}`)},
+    eat: function(){console.log(`${this.name} is eating ${this.favFood}.`)}
+}
+
+person1.eat();
+person2.eat();
+*/
+
+// constructor = special method for defining the
+//               properties and methods of objects
