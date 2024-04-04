8442525f803bca4932866c0e2ab193e3911badfb
Ryan Dempsey <rjd9896@gmail.com>
2024-04-05 00:21:15 +0900

    Update index.js

--- Notes/index.js
+++ Notes/index.js
@@ -729,4 +729,35 @@ function display(element){
 // .map() = accepts a callback and applies that function
 //          to each element of an array, then return a new array
 
-//
+// It's very similar to the forEach() method, however it returns
+// a new array, which is a key difference.
+
+/*
+const numbers = [1, 2, 3, 4, 5];
+const squares = numbers.map(square);
+const cubes = numbers.map(cube);
+
+console.log(squares);
+console.log(cubes);
+
+function square(element){
+    return Math.pow(element, 2);
+}
+
+function cube(element){
+    return Math.pow(element, 3);
+}
+*/
+const students = ["Spongebob", "Patrick", "Squidward", "Rick James"];
+const studentsUpper = students.map(upperCase);
+const studentsLower = students.map(lowerCase);
+
+console.log(studentsUpper)
+
+function upperCase(element){
+    return element.toUpperCase();
+}
+
+function lowerCase(element){
+    return element.toLowerCase();
+}
