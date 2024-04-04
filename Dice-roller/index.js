29b6392b7422f5659e785218225f6df5c7a37902
Ryan Dempsey <rjd9896@gmail.com>
2024-04-04 23:21:47 +0900

    edits

--- Dice-roller/index.js
+++ Dice-roller/index.js
@@ -5,14 +5,21 @@ function rollDice(){
     const numOfDice = document.getElementById("numOfDice").value;
     const diceResult = document.getElementById("diceResult");
     const diceImages = document.getElementById("diceImages");
+    const diceSum = document.getElementById("diceSum");
     const values = [];
     const images = [];
+    let sum = 0;
 
     for(let i = 0; i < numOfDice; i++){
         const value = Math.floor(Math.random() * 6) + 1;
         values.push(value);
         images.push(`<img src="images/dice-${value}.png">`)
     }
+
+    for(let i = 0; i < values.length; i++){
+        sum += values[i];
+    }
     diceResult.textContent = `dice: ${values.join(', ')}`;
     diceImages.innerHTML = images.join('')
+    diceSum.textContent = `Total Sum: ${sum}`;
 }
