d1c599a4f46cffd9875775a97382104787b1c861
Ryan Dempsey <rjd9896@gmail.com>
2024-04-05 00:02:36 +0900

    Update index.js

--- Notes/index.js
+++ Notes/index.js
@@ -694,3 +694,11 @@ function displayConsole(result){
 //             to each element.
 
 //             array.forEach(callback)
+
+let numbers = [1, 2, 3, 4, 5];
+
+numbers.forEach(display);
+
+function display(element){
+    console.log(element);
+}
