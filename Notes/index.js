206621d5ebda2e39797edcd2ef9f728d5f4ee010
Ryan Dempsey <rjd9896@gmail.com>
2024-04-05 01:11:11 +0900

    Update index.js

--- Notes/index.js
+++ Notes/index.js
@@ -879,8 +879,10 @@ const squares = numbers.map((element) => Math.pow(element, 2));
 const cubes = numbers.map((element) => Math.pow(element, 3));
 const evenNums = numbers.filter((element) => element % 2 === 0);
 const oddNums = numbers.filter((element) => element % 2 !== 0);
+const total = numbers.reduce((accumulator, element) => accumulator + element);
 
 console.log(squares);
 console.log(cubes);
 console.log(evenNums);
 console.log(oddNums);
+console.log(total);
