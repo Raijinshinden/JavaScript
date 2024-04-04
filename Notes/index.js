bebd228ba0ba0249ccf35371e02eec942ae3daa6
Ryan Dempsey <rjd9896@gmail.com>
2024-04-04 23:53:00 +0900

    Update index.js

--- Notes/index.js
+++ Notes/index.js
@@ -646,4 +646,32 @@ console.log(foods);
 */
 
 
-//
+// callback = a function that is passed as an argument
+//            to another function.
+
+//            used to handle asynchronous operations:
+//            1. Reading a file
+//            2. Network requests
+//            3. Interacting with databases
+
+//            "Hey, when you're done, call this next."
+
+hello(wait);
+
+
+function hello(callback){
+    console.log("Hello");
+    callback();
+}
+
+function wait(){
+    console.log("Wait!");
+}
+
+function leave(){
+    console.log("Leave!");
+}
+
+function goodbye(){
+    console.log("Goodbye");
+}
