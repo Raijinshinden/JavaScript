5618143782ac646e8c2fd343feb8039ee5e7ee42
Ryan Dempsey <rjd9896@gmail.com>
2024-04-06 00:11:40 +0900

    Update index.js

--- Notes/index.js
+++ Notes/index.js
@@ -977,3 +977,23 @@ console.log(car3.year);
 console.log(car3.color);
 car3.drive();
 */
+
+// class = (ES6 feature) provides a more structured and cleaner way to 
+//          work with objects compared to traditional constructor functions
+//          ex. static keyword, encapsulation, inheritance.
+
+class Product{
+    constructor(name, price){
+        this.name = name;
+        this.price = price;
+    }
+
+    displayProduct(){
+        console.log(`Product: ${this.name}`)
+        console.log(`Price: $${this.price}`)
+    }
+}
+
+const product1 = new Product("Shirt", 19.99);
+
+product1.displayProduct();
