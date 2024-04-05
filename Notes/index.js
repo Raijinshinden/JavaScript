50f416f3c7f733ff14f401c62d394eb9933ef35f
Ryan Dempsey <rjd9896@gmail.com>
2024-04-06 00:18:23 +0900

    Update index.js

--- Notes/index.js
+++ Notes/index.js
@@ -990,10 +990,23 @@ class Product{
 
     displayProduct(){
         console.log(`Product: ${this.name}`)
-        console.log(`Price: $${this.price}`)
+        console.log(`Price: $${this.price.toFixed(2)}`)
+    }
+
+    calculateTotal(salesTax){
+        return this.price + (this.price * salesTax);
     }
 }
 
+const salesTax = 0.05;
+
 const product1 = new Product("Shirt", 19.99);
+const product2 = new Product("Pants", 22.50);
+const product3 = new Product("Underwear", 100);
 
 product1.displayProduct();
+product2.displayProduct();
+product3.displayProduct();
+
+const total = product2.calculateTotal(salesTax);
+console.log(`Total price (with tax): $${total.toFixed(2)}`);
