dc2fae3ddbc1e2516226709290a048178d25a56e
Ryan Dempsey <rjd9896@gmail.com>
2024-04-07 00:33:04 +0900

    Update index.js

--- Notes/index.js
+++ Notes/index.js
@@ -1381,8 +1381,8 @@ const person3 = new Person("Squidward", 45, "126 Conch St",
 
 // Array of Objects //
 
-const fruits =[{},
-               {},
-               {},
-               {},
-               {}];
+const fruits =[{name: "apple", color: "red", calories: 95},
+               {name: "orange", color: "orange", calories: 45},
+               {name: "banana", color: "yellow", calories: 105},
+               {name: "coconut", color: "white", calories: 159},
+               {name: "pineapple", color: "yellow", calories: 37}];
