45c534f447b8a6b2ab725f3e9439339b37339b0e
Ryan Dempsey <rjd9896@gmail.com>
2024-04-07 01:20:04 +0900

    Update index.js

--- Notes/index.js
+++ Notes/index.js
@@ -1449,7 +1449,7 @@ console.log(people);
 */
 
 // Shuffle an Array //
-
+/*
 const cards = ['A', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K'];
 shuffle(cards);
 console.log(cards);
@@ -1463,3 +1463,7 @@ function shuffle(array){
         [array[i], array[random]] = [array[random], array[i]];
     }
 }
+*/
+
+// Data Objects = Objects that contain values that represent dates and times
+//                These date objects can be changed and formatted.
