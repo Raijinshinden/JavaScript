e541877974f485533b5f0306dbdd98af0b275d89
Ryan Dempsey <rjd9896@gmail.com>
2024-04-07 01:52:07 +0900

    Update index.js

--- Notes/index.js
+++ Notes/index.js
@@ -1467,7 +1467,7 @@ function shuffle(array){
 
 // Data Objects = Objects that contain values that represent dates and times
 //                These date objects can be changed and formatted.
-
+/*
 // Date(year, month, day, hour, minute, second, ms)
 const date = new Date();
 
@@ -1493,3 +1493,74 @@ date.setDate(7);
 date.setHours(1)
 date.setMinutes(31);
 console.log(date);
+*/
+
+// Closure = A function defined inside of another function,
+//           the inner function has access to the variables
+//           and scope of the outer function.
+//           Allow for private variables and state maintenance
+//           Used frequently in JS frameworks: React, Vue, Angular
+
+// function outer(){
+
+//     let message = "Hello";
+
+//     function inner(){
+//         console.log(message);
+//     }
+//     inner()
+// }
+
+// outer();
+
+// function createCounter(){
+
+//     let count = 0;
+
+//     function increment(){
+//         count++;
+//         console.log(`Count increased to ${count}`);
+//     }
+
+//     function getCount(){
+//         return count;
+//     }
+
+//     return {increment, getCount};
+// }
+
+// const counter = createCounter();
+
+// counter.increment();
+// counter.increment();
+// counter.increment();
+// counter.increment();
+
+// console.log(`The current count is ${counter.getCount()}`)
+
+function createGame(){
+    let score = 0;
+
+    function increaseScore(points){
+        score += points;
+        console.log(`+${points}pts`)
+    }
+    
+    function decreaseScore(points){
+        score -= points;
+        console.log(`-${points}pts`)
+    }
+    
+    function getScore(){
+        return score;
+    } 
+
+    return {increaseScore, decreaseScore, getScore};
+}
+
+const game = createGame();
+
+game.increaseScore(8);
+game.increaseScore(2);
+game.decreaseScore(2);
+console.log(`The final score is ${game.getScore()}pts`);
