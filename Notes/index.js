25b1b29707e625c4b66cb34b61138759b2de7ba8
Ryan Dempsey <rjd9896@gmail.com>
2024-04-07 22:05:34 +0900

    Update index.js

--- Notes/index.js
+++ Notes/index.js
@@ -2030,3 +2030,57 @@ const myButton = document.getElementById("myButton");
 //2. CLEAN THE KITCHEN
 //3. TAKE OUT THE TRASH
 
+function walkDog(){
+
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+
+            const dogWalked = true;
+
+            if(dogWalked){
+                resolve("You walk the dog. 🐕")
+            }
+            else{
+                reject("You didn't walk the dog!")
+            }
+            
+        }, 1500)    
+    });
+}
+
+function cleanKitchen(){
+
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+
+            const kitchenCleaned = true;
+
+            if(kitchenCleaned){
+                resolve("You clean the kitchen.🧹")
+            }
+            else{
+                reject("You didn't clean the kitchen.")
+            }
+
+            
+        }, 2500)
+    });
+}
+
+function takeOutTrash(){
+
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+
+            const 
+
+            resolve("You take out the trash.🗑️")
+        }, 500)
+    })
+}
+
+
+// Method Chaining
+walkDog().then(value => {console.log(value); return cleanKitchen()})
+         .then(value => {console.log(value); return takeOutTrash()})
+         .then(value => {console.log(value); console.log("You finished all the chores!")});
