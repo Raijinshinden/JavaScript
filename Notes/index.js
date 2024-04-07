5c667118bf5547e204a688697d6711828226fdfc
Ryan Dempsey <rjd9896@gmail.com>
2024-04-07 14:02:08 +0900

    Update index.js

--- Notes/index.js
+++ Notes/index.js
@@ -1787,6 +1787,22 @@ console.log(date);
 
 const myBox = document.getElementById("myBox");
 
+// document.addEventListener("keydown", event => {
+//     myBox.textContent = "😮";
+//     myBox.style.backgroundColor = "tomato";
+// })
+
+// document.addEventListener("keyup", event => {
+//     myBox.textContent = "😁";
+//     myBox.style.backgroundColor = "lightblue";
+// })
+
+const moveAmount = 10;
+let x = 0;
+let y = 0;
+
+
+
 document.addEventListener("keydown", event => {
     myBox.textContent = "😮";
     myBox.style.backgroundColor = "tomato";
@@ -1795,4 +1811,30 @@ document.addEventListener("keydown", event => {
 document.addEventListener("keyup", event => {
     myBox.textContent = "😁";
     myBox.style.backgroundColor = "lightblue";
+})
+
+document.addEventListener("keydown", event => {
+
+    if(event.key.startsWith("Arrow")){
+
+        event.preventDefault();
+
+        switch(event.key){
+            case "ArrowUp":
+                y -= moveAmount;
+                break;
+            case "ArrowDown":
+                y += moveAmount;
+                break;
+            case "ArrowLeft":
+                x -= moveAmount;
+                break;
+            case "ArrowRight":
+                x += moveAmount;
+                break;    
+        }
+
+        myBox.style.top = `${y}px`;
+        myBox.style.left = `${x}px`;
+    }
 })
