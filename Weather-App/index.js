b3ce400fe32cb23ddda4835308f0f56d231b5a1f
Ryan Dempsey <rjd9896@gmail.com>
2024-04-08 00:24:05 +0900

    Update index.js

--- Weather-App/index.js
+++ Weather-App/index.js
@@ -4,14 +4,21 @@ const cityInput = document.querySelector(".cityInput");
 const card = document.querySelector(".card");
 const apiKey = "d7a3239fde33c2c92c698c88d919cd62";
 
-weatherForm.addEventListener("submit", event => {
+weatherForm.addEventListener("submit", async event => {
 
     event.preventDefault();
 
     const city = cityInput.value;
 
     if(city){
-
+        try{
+            const weatherData = await getWeatherData(city);
+            displayWeatherInfo(weatherData);
+        }
+        catch(error){
+            console.error(error);
+            displayError(error);
+        }
     }
     else{
         displayError("Please enter a city");
@@ -21,11 +28,21 @@ weatherForm.addEventListener("submit", event => {
 
 async function getWeatherData(city){
 
+    const apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
 
+    const response = await fetch(apiURL);
+
+    if(!response.ok){
+        throw new Error("Could not fetch weather data");
+    }
+    return await response.json();
 }
 
 function displayWeatherInfo(data){
 
+    const {name: city, 
+           main: {temp, humdity}, 
+           weather: [{description, id}]} = data;
 }
 
 function getWeatherEmoji(weatherId){
