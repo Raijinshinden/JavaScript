60ee9dcd596d63613740655f9b068488033d17e4
Ryan Dempsey <rjd9896@gmail.com>
2024-04-07 15:08:18 +0900

    edit

--- Notes/index.js
+++ Notes/index.js
@@ -1935,10 +1935,22 @@ const myButton = document.getElementById("myButton");
 // myButton.classList.add("enabled");
 // myButton.classList.remove("enabled");
 
-myButton.addEventListener("mouseover", event => {
-    event.target.classList.add("hover");
-})
+// myButton.addEventListener("mouseover", event => {
+//     event.target.classList.toggle("hover");
+// })
 
-myButton.addEventListener("mouseout", event => {
-    event.target.classList.remove("hover");
-})
+// myButton.addEventListener("mouseout", event => {
+//     event.target.classList.toggle("hover");
+// })
+
+myButton.classList.add("enabled");
+
+myButton.addEventListener("click", event => {
+
+    if(event.target.classList.contains("disabled")){
+        event.target.textContent += "🤬";
+    }
+    else{
+        event.target.classList.replace("enabled", "disabled");
+    }
+});
