01b1d03ddcb97d5dd1d4677ac497ee0942f62120
Ryan Dempsey <rjd9896@gmail.com>
2024-04-07 14:42:05 +0900

    edits

--- Notes/index.js
+++ Notes/index.js
@@ -1867,6 +1867,52 @@ const myBox = document.getElementById("myBox");
 
 let buttons = document.querySelectorAll(".myButtons");
 
-console.log(buttons);
-
 // ADD HTML/CSS PROPERTIES
+
+// buttons.forEach(button => {
+//     button.style.backgroundColor = "green";
+//     button.textContent += "😊";
+// });
+
+// CLICK event listener
+
+// buttons.forEach(button => {
+//     button.addEventListener("click", event => {
+//         event.target.style.backgroundColor = "tomato";
+//     })
+// });
+
+// MOUSEOVER + MOUSEOUT event listener
+
+// buttons.forEach(button => {
+//     button.addEventListener("mouseover", event => {
+//         event.target.style.backgroundColor = "hsl(205, 100%, 40%)"
+//     })
+// })
+
+// buttons.forEach(button => {
+//     button.addEventListener("mouseout", event => {
+//         event.target.style.backgroundColor = "hsl(205, 100%, 60%)"
+//     })
+// })
+
+
+// ADD AN ELEMENT
+
+// const newButton = document.createElement("button"); //STEP 1
+// newButton.textContent = "Button 5" //STEP 2
+// newButton.classList = "myButtons";
+// document.body.appendChild(newButton)//STEP 3
+
+// buttons = document.querySelectorAll("myButtons");
+// Our Nodelist now has 5 buttons
+
+// REMOVE AN ELEMENT
+
+buttons.forEach(button => {
+    button.addEventListener("click", event => {
+        event.target.remove();
+        buttons = document.querySelectorAll(".myButtons");
+        console.log(buttons);
+    })
+})
